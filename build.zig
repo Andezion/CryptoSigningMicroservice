c2685dd4541966ea26603aa5621dda36633fa3c4
Andezion <245122@edu.p.lodz.pl>
2026-01-09 12:42:15 +0100

    this should work

--- build.zig
+++ build.zig
@@ -6,10 +6,13 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "cryptosign",
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-    exe.root_module.root_source_file = b.path("src/main.zig");
-    exe.root_module.resolved_target = target;
-    exe.root_module.optimize = optimize;
+    // exe.root_module.root_source_file = b.path("src/main.zig");
+    // exe.root_module.resolved_target = target;
+    // exe.root_module.optimize = optimize;
 
     b.installArtifact(exe);
 
