7a8237f70f39ff32b106a37aeb26085341741984
Andezion <245122@edu.p.lodz.pl>
2026-01-09 12:43:59 +0100

    pleeease work i beg u

--- build.zig
+++ build.zig
@@ -4,12 +4,9 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "cryptosign",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    const exe = b.addExecutable("cryptosign", b.path("src/main.zig"));
+    exe.setTarget(target);
+    exe.setOptimize(optimize);
     // exe.root_module.root_source_file = b.path("src/main.zig");
     // exe.root_module.resolved_target = target;
     // exe.root_module.optimize = optimize;
