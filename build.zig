5bf85451965c3042c23e690f14e974565c7f5225
Andezion <245122@edu.p.lodz.pl>
2026-01-09 12:37:49 +0100

    fixes for cicd

--- build.zig
+++ build.zig
@@ -6,10 +6,12 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "cryptosign",
+        .root_module = .{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        },
     });
-    exe.root_module.root_source_file = b.path("src/main.zig");
-    exe.root_module.target = target;
-    exe.root_module.optimize = optimize;
 
     b.installArtifact(exe);
 
@@ -23,10 +25,13 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the crypto signing service");
     run_step.dependOn(&run_cmd.step);
 
-    const unit_tests = b.addTest(.{});
-    unit_tests.root_module.root_source_file = b.path("src/main.zig");
-    unit_tests.root_module.target = target;
-    unit_tests.root_module.optimize = optimize;
+    const unit_tests = b.addTest(.{
+        .root_module = .{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+        },
+    });
 
     const run_unit_tests = b.addRunArtifact(unit_tests);
     const test_step = b.step("test", "Run unit tests");
